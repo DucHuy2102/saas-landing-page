7854f50aa31da211a7c5f1749a82e641a6ae0a0c
Duc Huy Nguyen <duchuytv2102@gmail.com>
2025-03-08 16:18:50 +0700

    complete the introduction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Hero, LogoTicker, Navbar } from '@/sections';
+import { Hero, Introduction, LogoTicker, Navbar } from '@/sections';
 
 export default function Home() {
     return (
@@ -6,6 +6,7 @@ export default function Home() {
             <Navbar />
             <Hero />
             <LogoTicker />
+            <Introduction />
         </>
     );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as C_Button } from './Button';
 export { default as C_Pointer } from './Pointer';
+export { default as C_Tag } from './Tag';
